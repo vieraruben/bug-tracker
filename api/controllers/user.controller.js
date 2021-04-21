3f0b64c0755363a0e11e780bdd156e7e7ee8654e
Ruben Viera <viera.ruben@outlook.com>
2021-04-21 07:51:54 -0500

    adding user and auth routes, controllers and models

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,86 @@
+const User = require('../models/user.model')
+// const Product = require('../models/product.model')
+const extend = require('lodash/extend')
+const errorHandler = require('./../helpers/dbErrorHandler')
+const config = require('../../config/config')
+
+const create = async (req, res) => {
+  const user = new User(req.body)
+  try {
+    await user.save()
+    return res.status(200).json({
+      message: "Successfully signed up!"
+    })
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
+
+/**
+ * Load user and append to req.
+ */
+const userByID = async (req, res, next, id) => {
+  try {
+    let user = await User.findById(id)
+    if (!user)
+      return res.status('400').json({
+        error: "User not found"
+      })
+    req.profile = user
+    next()
+  } catch (err) {
+    return res.status('400').json({
+      error: "Could not retrieve user"
+    })
+  }
+}
+
+const read = (req, res) => {
+  req.profile.hashed_password = undefined
+  req.profile.salt = undefined
+  return res.json(req.profile)
+}
+
+const update = async (req, res) => {
+  try {
+    let user = req.profile
+    user = extend(user, req.body)
+    user.updated = Date.now()
+    await user.save()
+    user.hashed_password = undefined
+    user.salt = undefined
+    res.json(user)
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
+
+const remove = async (req, res) => {
+  try {
+    let user = req.profile
+    // Deactivate all user's products
+    // await Product.updateMany({"createdBy": req.profile._id}, {"$set":{"active": false}}).exec();
+
+    // Remove user
+    let deletedUser = await user.remove()
+    deletedUser.hashed_password = undefined
+    deletedUser.salt = undefined
+    res.json(deletedUser)
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
+}
+
+module.exports = {
+  create,
+  userByID,
+  read,
+  remove,
+  update
+}
--- server.js
+++ server.js
@@ -1,63 +1,22 @@
-// const createError = require('http-errors');
-// const express = require('express');
-// const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
 
-// const port = process.env.PORT || 3000;
+const config = require('./config/config')
+const mongoose = require('mongoose')
+const app = require('./express')
 
-// const indexRouter = require('./api/routes/index');
-// const usersRouter = require('./api/routes/users');
+mongoose.Promise = global.Promise
 
-// const app = express();
+mongoose.connect(config.mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`)
+})
+  .catch(err => console.log(err))
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-
-const indexRouter = require('./api/routes/index');
-const usersRouter = require('./api/routes/users');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-
-app.listen(port);
-
-console.log('todo list RESTful API server started on: ' + port);
+app.listen(config.port, (err) => {
+  if (err) console.log(err)
+  console.info('Server started on port %s.', config.port)
+})
