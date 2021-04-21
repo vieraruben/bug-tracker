41488169219a47c8c465edea85aac985fc7c7870
Ruben Viera <viera.ruben@outlook.com>
2021-04-21 08:25:47 -0500

    adding new heroku configuration

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,35 +4,34 @@ const expressJwt = require('express-jwt')
 const config = require('../../config/config')
 
 const signin = async (req, res) => {
-  console.log('here')
   try {
     let user = await User.findOne({
-        "email": req.body.email
+      "email": req.body.email
+    })
+
+    if (!user)
+      return res.status('401').json({
+        error: "User not found"
       })
 
-      if (!user)
-        return res.status('401').json({
-          error: "User not found"
-        })
-
-      if (!user.authenticate(req.body.password)) {
-        return res.status('401').send({
-          error: "Email and password don't match."
-        })
-      }
-
-      const token = jwt.sign({
-        _id: user._id
-      }, config.jwtSecret)
-
-      res.cookie("t", token, {
-        expire: new Date() + 9999
+    if (!user.authenticate(req.body.password)) {
+      return res.status('401').send({
+        error: "Email and password don't match."
       })
+    }
 
-      return res.json({
-        token,
-        user
-      })
+    const token = jwt.sign({
+      _id: user._id
+    }, config.jwtSecret)
+
+    res.cookie("t", token, {
+      expire: new Date() + 9999
+    })
+
+    return res.json({
+      token,
+      user
+    })
   } catch (err) {
     return res.status('401').json({
       error: "Could not sign in"
